4fc33c2202b395cf9d11533f69f462413a3f757e
Jaiany Oliveira Silva <131549653+JaianyOS@users.noreply.github.com>
2024-10-19 20:38:12 -0300

    add service

--- src/main/java/service/TeamService.java
+++ src/main/java/service/TeamService.java
@@ -1,4 +1,36 @@
 package service;
 
+import controllers.DTOs.TeamUpdateDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repositories.TeamRepository;
+
+@Service
 public class TeamService {
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public Team findTeamById(Long teamId) {
+        return teamRepository.findById(teamId).orElseThrow(() -> new RuntimeException("Team not found"));
+    }
+
+    public void createTeam(Team team) {
+        teamRepository.save(team);
+    }
+
+    public void updateTeam(Long teamId, TeamUpdateDTO teamUpdateDTO) {
+        Team team = findTeamById(teamId);
+        team.setName(teamUpdateDTO.getName());
+        team.setPlayers(teamUpdateDTO.getPlayers());
+        teamRepository.save(team);
+    }
+    public void deleteTeam(Long teamId) {
+        if (teamRepository.existsById(teamId)) {
+            teamRepository.deleteById(teamId);
+        } else {
+            throw new RuntimeException("Team not found");
+        }
+    }
+
 }
