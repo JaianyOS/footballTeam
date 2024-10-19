9232d98394becbfc000dad50d8c0cab6de2bf7e7
Jaiany Oliveira Silva <131549653+JaianyOS@users.noreply.github.com>
2024-10-19 20:37:08 -0300

    add mapper

--- src/main/java/service/mapper/TeamMapper.java
+++ src/main/java/service/mapper/TeamMapper.java
@@ -1,4 +1,22 @@
 package service.mapper;
 
+import controllers.DTOs.TeamRegisterDTO;
+import controllers.DTOs.TeamResponseDTO;
+import models.Team;
+
 public class TeamMapper {
+    public static Team toTeam(TeamRegisterDTO teamRegisterDTO) {
+        Team team = new Team();
+        team.setName(teamRegisterDTO.getName());
+        team.setPlayers(teamRegisterDTO.getPlayers());
+        return team;
+    }
+
+    public static TeamResponseDTO toTeamResponseDto(Team team) {
+        TeamResponseDTO teamResponseDTO = new TeamResponseDTO();
+        teamResponseDTO.setId(team.getId());
+        teamResponseDTO.setName(team.getName());
+        teamResponseDTO.setPlayers(team.getPlayers());
+        return teamResponseDTO;
+    }
 }
