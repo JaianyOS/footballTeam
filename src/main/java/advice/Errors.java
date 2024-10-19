67a6d6ee7295b7b859bc96bcd1009c41330ff56e
Jaiany Oliveira Silva <131549653+JaianyOS@users.noreply.github.com>
2024-10-19 20:44:02 -0300

    add advice

--- src/main/java/advice/Errors.java
+++ src/main/java/advice/Errors.java
@@ -1,4 +1,23 @@
 package advice;
 
 public class Errors {
+    private String field;
+    private String message;
+    public Errors(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+    public String getField() {
+        return field;
+    }
+    public void setField(String field) {
+        this.field = field;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 }
--- src/main/java/advice/TeamAdvice.java
+++ src/main/java/advice/TeamAdvice.java
@@ -1,4 +1,24 @@
-package advice;
+import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+import java.util.ArrayList;
+
+@RestControllerAdvice
 public class TeamAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<ValidationErrors> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        List<ValidationErrors> errors = new ArrayList<>();
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            String field = fieldError.getField();
+            String message = fieldError.getDefaultMessage();
+            errors.add(new ValidationErrors(field, message));
+        });
+        return errors;
+    }
 }
