405ae5b006f3d84cb8fe720ce0a205dea29d0852
Jaiany Oliveira Silva <131549653+JaianyOS@users.noreply.github.com>
2024-10-19 20:29:12 -0300

    add model

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,21 @@
-package models;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
+
+import java.util.List;
+
+@Entity
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "O nome do time não pode ser vazio")
+    private String name;
+
+    @ElementCollection
+    @Size(max = 5, message = "O time não pode ter mais de 5 jogadores")
+    @NotEmpty(message = "A lista de jogadores não pode ser vazia")
+    private List<@NotEmpty(message = "O nome do jogador não pode ser vazio") String> players;
 }
